9e3807750a2ab74083860ed956830173845b2d4a
Julian <jpereyra2@csub.edu>
2022-02-24 12:31:22 -0800

    basic functions

--- jpereyra.cpp
+++ jpereyra.cpp
@@ -1,1 +1,15 @@
-//hello
+//Julian Pereyra 
+//Source file for Space Shooter 7
+//Start Date: February 24th
+//End Date: TBD
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Nothing to see here yet! Come back later. Code in progress . . .\n";
+
+    return 0;
+
+}
